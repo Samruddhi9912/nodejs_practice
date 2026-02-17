352c6aab9f06d6ad7423bbdb8cf1ccf8d9b3b628
Samruddhi KS <samruddhiyadiyal@gmail.com>
2026-02-17 19:42:22 +0200

    17-02-2026

---
+++ Authentication/src/routes/post.routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.post("/create", (req,res)=> {
+
+    console.log(req.body)
+
+    console.log(req.cookies)
+
+    res.send("Post created sucessfully")
+
+})
+
+module.exports = router
